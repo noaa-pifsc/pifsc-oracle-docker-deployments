5184723e23c6006d2e366fc8dea5f73f9c1bf4e6
Jesse Abdul <jesse.abdul@noaa.gov>
2026-01-14 15:58:48 -1000

    Updated diagram and host functions to change references from "docker" to the more general term "container"

--- src/functions/host_functions.sh
+++ src/functions/host_functions.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
-# function to initialize the docker target folder (where the docker project will be built/run) and build/run the container.  This function is run to build and run the container. This function accepts 2 parameters:
-# 1: the full path to the docker source directory
-# 2: the path of the docker compose file (relative to the docker source directory)
+# function to initialize the container target folder (where the container project will be built/run) and build/run the container.  This function is run to build and run the container. This function accepts 2 parameters:
+# 1: the full path to the container source directory
+# 2: the path of the container compose file (relative to the container_database_deployment source directory)
 # Example Usage:  
-# initialize_run_docker_project "/tmp/lhp-data-management-deploy/docker" "./docker-compose.yml"
-function initialize_run_docker_project ()
+# initialize_run_container_project "/tmp/lhp-data-management-deploy/container_database_deployment" "./docker-compose.yml"
+function initialize_run_container_project ()
 {
- 	echo "Change to the docker directory and build/run the container"
+ 	echo "Change to the container directory and build/run the container"
 
-	local docker_host_container_source_path="$1"
-	local docker_compose_file_path="$2"
+	local CONTAINER_HOST_SOURCE_PATH="$1"
+	local container_compose_file_path="$2"
 
 	# input validation
-    if [[ -z "$docker_host_container_source_path" || -z "$docker_compose_file_path" ]]; then
-        echo "ERROR: shutdown_cleanup_docker_project() requires the full path to the docker source directory and the path to the docker compose file as arguments" >&2
+    if [[ -z "$CONTAINER_HOST_SOURCE_PATH" || -z "$container_compose_file_path" ]]; then
+        echo "ERROR: shutdown_cleanup_container_project() requires the full path to the container source directory and the path to the container compose file as arguments" >&2
         return 1
     fi
 
-	# change to the docker container directory
-	cd ${docker_host_container_source_path}
+	# change to the container container directory
+	cd ${CONTAINER_HOST_SOURCE_PATH}
 
-	# build and run the sqlplus docker container
-	echo "build and run the sqlplus docker container"
-	build_deploy_container ${docker_compose_file_path}
+	# build and run the sqlplus container
+	echo "build and run the sqlplus container"
+	build_deploy_container ${container_compose_file_path}
 }
 
-# function to shutdown the docker container and cleanup docker target folder after the container scripts have been executed. This function is run to shutdown the container. This function accepts 2 parameters:
+# function to shutdown the container and cleanup the container target folder after the container scripts have been executed. This function is run to shutdown the container. This function accepts 2 parameters:
 # 1: the name of the configuration data variable used to store the STDIN data
-# 2: the path of the docker compose file (relative to the docker source directory - see docker_host_container_source_path in initialize_run_docker_project())
+# 2: the path of the container compose file (relative to the container_database_deployment source directory - see CONTAINER_HOST_SOURCE_PATH in initialize_run_container_project())
 # Example Usage: 
-# shutdown_cleanup_docker_project "CONFIG_DATA" "./docker-compose.yml" 
-function shutdown_cleanup_docker_project ()
+# shutdown_cleanup_container_project "CONFIG_DATA" "./docker-compose.yml" 
+function shutdown_cleanup_container_project ()
 {
 	local config_data_var="$1"
-	local docker_compose_file_path="$2"
+	local container_compose_file_path="$2"
 
 	# input validation
-    if [[ -z "$config_data_var" || -z "$docker_compose_file_path" ]]; then
-        echo "ERROR: shutdown_cleanup_docker_project() requires the name of the configuration data variable and the path to the docker compose file as arguments" >&2
+    if [[ -z "$config_data_var" || -z "$container_compose_file_path" ]]; then
+        echo "ERROR: shutdown_cleanup_container_project() requires the name of the configuration data variable and the path to the container compose file as arguments" >&2
         return 1
     fi
 
-	echo "shutdown the docker container and cleanup docker target folder"
+	echo "shutdown the container and cleanup the container target folder"
 
 	# unset the variable named $config_data_var
 	unset_config_data "$config_data_var"
 
 	# when the deployment has been completed, shutdown the container
- 	shutdown_container ${docker_compose_file_path}
+ 	shutdown_container ${container_compose_file_path}
 }
 
