71f4399d3fbd66fa8e484e9c4cc117cde27ba99b
Jesse Abdul <jesse.abdul@noaa.gov>
2026-01-28 16:21:23 -1000

    Updated client_functions.sh to specify a variable from an argument instead of calling a function that is defined in the parent repository

--- src/functions/client_functions.sh
+++ src/functions/client_functions.sh
@@ -34,6 +34,7 @@ function set_script_type_var ()
 # 12: the database service name
 # 13: the name of the associative array containing the secret names and corresponding bash variables
 # 14: the path to the container's bash scripts folder
+# 15: (optional) a formatted list of custom export commands that will precede the bash script call to define any environment variables that are necessary for the bash script
 # Example Usage:
 #   prepare_execute_deployment_script "$1" "$2" "$3" "docker-server-as1" "/tmp/lhp-data-management-deploy" "git@github.com:example-repo.git" "CONFIG_DATA" "/tmp/lhp-data-management-deploy/container_database_deployment/host_scripts" "/c/Users/USERNAME/lhp-data-management/container_database_deployment" "./docker-compose.yml" "deploy_version2.0" "oracle-db-host" "oracle-db-servicename" "SECRET_MAPPING_ARR"
 function prepare_execute_deployment_script ()
@@ -52,11 +53,12 @@ function prepare_execute_deployment_script ()
 	local script_type="${10}"
 	local db_host="${11}"
 	local db_service_name="${12}"
-	local SECRET_MAPPING_VAR_NAME="${13}"
+	local secret_mapping_var_name="${13}"
 	local container_scripts_path="${14}"
+	local env_vars_block="${15:-}"
 	
 	# input validation
-    if [[ -z "${env_name}" || -z "${deployment_destination}" || -z "${container_hostname}" || -z "${container_host_project_path}" || -z "${container_git_url}" || -z "${config_data_var_name}" || -z "${container_host_scripts_path}" || -z "${local_container_build_path}" || -z "${container_compose_file_path}" || -z "${script_type}" || -z "${db_host}" || -z "${db_service_name}" || -z "${SECRET_MAPPING_VAR_NAME}" || -z "${container_scripts_path}" ]]; then
+    if [[ -z "${env_name}" || -z "${deployment_destination}" || -z "${container_hostname}" || -z "${container_host_project_path}" || -z "${container_git_url}" || -z "${config_data_var_name}" || -z "${container_host_scripts_path}" || -z "${local_container_build_path}" || -z "${container_compose_file_path}" || -z "${script_type}" || -z "${db_host}" || -z "${db_service_name}" || -z "${secret_mapping_var_name}" || -z "${container_scripts_path}" ]]; then
         echo "ERROR: prepare_execute_deployment_script() requires the environment name, the deployment destination, the container hostname to connect to, the container source directory on the container host, git url for the container project's repository, name of the configuration data variable, the path to the folder where the host bash scripts are contained, the local container build folder path (/container_database_deployment), the path of the container compose file (relative to the container build folder path), the script type, the database host, the database service name, the name of an associative array that maps the secret values passed to bash commands via STDIN, and the path to the container's bash scripts folder as arguments" >&2
         return 1
     fi
@@ -88,7 +90,7 @@ function prepare_execute_deployment_script ()
 		build_deploy_container "${container_compose_file_path}"
 
 		# execute the corresponding container scripts and shutdown the container
-		execute_container_scripts "${container_scripts_path}" "${container_compose_file_path}" "${config_data_var_name}"  "$(host_deploy_define_env_vars_block)"
+		execute_container_scripts "${container_scripts_path}" "${container_compose_file_path}" "${config_data_var_name}"  "$(env_vars_block)"
 
 		echo "the local container deployment script has finished executing"
 
