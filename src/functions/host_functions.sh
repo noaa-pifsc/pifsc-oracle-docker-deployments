f846e667256f704d63a267e46b0f57de7e8461cc
Jesse Abdul <jesse.abdul@noaa.gov>
2026-01-29 15:35:14 -1000

    Consolidated more login in the host_functions.sh so more code can be reused across data systems

--- src/functions/host_functions.sh
+++ src/functions/host_functions.sh
@@ -51,28 +51,96 @@ function host_shutdown_container_elev_privs ()
  	shutdown_container "${container_compose_file_path}"
 }
 
-
-# function to build/run the container using the ${container_account_name} account. This function accepts the following parameters:
-# 1: the container source directory on the container host
-# 2: the name of a container privileged user account that can run container commands
-# 3: the path to the folder where the host bash scripts are contained  
+# function to run the oracle database scripts within the running container. This function accepts the following parameters:
+# 1: the full path of the calling script
+# 2: the path to the container's bash scripts folder
+# 3: the path of the container compose file
 # 4: name of the configuration data variable
-# 5: name of the host script that is executed to deploy the container
-# 6: (optional) a formatted list of custom export commands that will precede the bash script call to define any environment variables that are necessary for the bash script
-function host_deploy_container ()
+# 5: the full path to the container source directory
+# 6: the name of the associative array containing the secret names and corresponding bash variables
+# 7: (optional) a formatted list of custom export commands that will precede the bash script call to define any environment variables that are necessary for the bash script
+function host_execute_container_script ()
 {
-	local container_host_project_path="${1}"
-	local container_account_name="${2}"
-	local container_host_scripts_path="${3}"
+	local current_script_name="${1}"
+	local container_scripts_path="${2}"
+	local container_compose_file_path="${3}"
 	local config_data_var_name="${4}"
-	local host_script_name="${5}"
-	local env_vars_block="${6:-}"
+	local container_host_source_path="${5}"
+	local secret_mapping_var_name="${6}"
+	local env_vars_block="${7:-}"
 
-	if [[ -z "${container_host_project_path}" || -z "${container_account_name}" || -z "${container_host_scripts_path}" || -z "${config_data_var_name}" || -z "${host_script_name}" ]]; then
-        echo "ERROR: host_deploy_container() requires the container source directory on the container host, the name of a container privileged user account that can run container commands, the path to the folder where the host bash scripts are contained, the name of the configuration data variable, and the host script name as arguments" >&2
+	if [[ -z "${current_script_name}" || -z "${container_scripts_path}" || -z "${container_compose_file_path}" || -z "${config_data_var_name}" || -z "${container_host_source_path}" || -z "${secret_mapping_var_name}" ]]; then
+        echo "ERROR: host_execute_container_script() requires the full path of the calling script, the path to the container's bash scripts folder, the path of the container compose file, the name of the configuration data variable, the full path to the container source directory, the name of the associative array containing the secret names and corresponding bash variables as arguments" >&2
         return 1
     fi
 
+	# initialize the container environment variables
+	initialize_container_env_var "${current_script_name}"
+
+	# initialize the container target folder and build/run the container
+	host_deploy_container_elev_privs "${container_host_source_path}" "${container_compose_file_path}"
+
+	# process the stdin configuration data: parse and store in variables, construct the formatted variable identified by $config_data_var_name
+	process_stdin_config_data "${secret_mapping_var_name}" "${config_data_var_name}"
+
+	# construct the full path to the script that will be executed within the container:
+	local script_path="${container_scripts_path}/container_${SCRIPT_TYPE}.sh"
+
+	echo "run the container_${SCRIPT_TYPE}.sh script from within the container to execute the corresponding automated scripts"
+
+# open a bash session into the running container and run the appropriate container deployment script (based on $SCRIPT_TYPE) and provide the value of the variable identified by $config_data_var_name via stdin
+docker exec -i oracle_deploy bash -c "
+	# specify the environment variables that are defined in the calling script:
+	${env_vars_block}
+	
+	# Execute the target script, which will inherit the variables above.
+	bash '${script_path}'
+" <<< "${!config_data_var_name}"
+
+	# shutdown and cleanup the container project
+	host_shutdown_container_elev_privs "${config_data_var_name}" "${container_compose_file_path}"
+}
+
+
+
+# function to initialize and run the database deployment container on the host machine. This function accepts the following parameters:
+# 1: the full path of the calling script
+# 2: the repository root folder (used to convert all .sh files to use linux-style line endings for compatibility purposes
+# 3: the name of the associative array containing the secret names and corresponding bash variables
+# 4: name of the configuration data variable
+# 5: the container source directory on the container host
+# 6: the name of a container privileged user account that can run container commands
+# 7: the path to the folder where the host bash scripts are contained
+# 8: name of the host script that is executed to deploy the container
+# 9: (optional) a formatted list of custom export commands that will precede the bash script call to define any environment variables that are necessary for the bash script
+function host_deploy_container ()
+{
+	local current_script_name="${1}"
+	local parent_root_folder="${2}"
+	local secret_mapping_var_name="${3}"
+	local config_data_var_name="${4}"
+	local container_host_project_path="${5}"
+	local container_account_name="${6}"
+	local container_host_scripts_path="${7}"
+	local host_script_name="${8}"
+	local env_vars_block="${9}"
+
+	# input validation
+    if [[ -z "${current_script_name}" || -z "${parent_root_folder}" || -z "${secret_mapping_var_name}" || -z "${config_data_var_name}" || -z "${container_host_project_path}" || -z "${container_account_name}" || -z "${container_host_scripts_path}" || -z "${host_script_name}" ]]; then
+        echo "ERROR: host_initialize_deploy_container() requires the full path of the calling script, the repository root folder, the name of the associative array containing the secret names and corresponding bash variables, the name of the configuration data variable, the container source directory on the container host, the name of a container privileged user account that can run container commands, the path to the folder where the host bash scripts are contained, and name of the host script that is executed to deploy the container as arguments" >&2
+        return 1
+    fi
+
+	# initialize the container environment variables
+	initialize_container_env_var "${current_script_name}"
+
+	# convert the line endings for all .sh and .env files in the parent folder
+	convert_dos2unix "../../../"
+
+	# process the stdin configuration data: parse and store in variables, construct the formatted CONFIG_DATA variable
+	process_stdin_config_data "${secret_mapping_var_name}" "${config_data_var_name}"
+
+	# build/run the container
 	# define the absolute path to the deployment script that will run as ${container_account_name}.
 	local script_path="${container_host_scripts_path}/${host_script_name}"
 	
@@ -102,40 +170,4 @@ EOF
 
 	# cleanup the container source folder
 	cleanup_container_source_folder "${container_host_project_path}" "${config_data_var_name}"
-}
-
-
-# function to run the oracle database scripts within the running container. This function accepts the following parameters:
-# 1: the path to the container's bash scripts folder
-# 2: the path of the container compose file
-# 3: name of the configuration data variable
-# 4: (optional) a formatted list of custom export commands that will precede the bash script call to define any environment variables that are necessary for the bash script
-function host_execute_container_script ()
-{
-	local container_scripts_path="${1}"
-	local container_compose_file_path="${2}"
-	local config_data_var_name="${3}"
-	local env_vars_block="${4:-}"
-
-	if [[ -z "${container_scripts_path}" || -z "${container_compose_file_path}" || -z "${config_data_var_name}" ]]; then
-        echo "ERROR: host_execute_container_script() requires the path to the container's bash scripts folder, the path of the container compose file, and the name of the configuration data variable as arguments" >&2
-        return 1
-    fi
-
-	# construct the full path to the script that will be executed within the container:
-	local script_path="${container_scripts_path}/container_${SCRIPT_TYPE}.sh"
-
-	echo "run the container_${SCRIPT_TYPE}.sh script from within the container to execute the corresponding automated scripts"
-
-# open a bash session into the running container and run the appropriate container deployment script (based on $SCRIPT_TYPE) and provide the value of the variable identified by $config_data_var_name via stdin
-docker exec -i oracle_deploy bash -c "
-	# specify the environment variables that are defined in the calling script:
-	${env_vars_block}
-	
-	# Execute the target script, which will inherit the variables above.
-	bash '${script_path}'
-" <<< "${!config_data_var_name}"
-
-	# shutdown and cleanup the container project
-	host_shutdown_container_elev_privs "${config_data_var_name}" "${container_compose_file_path}"
 }
