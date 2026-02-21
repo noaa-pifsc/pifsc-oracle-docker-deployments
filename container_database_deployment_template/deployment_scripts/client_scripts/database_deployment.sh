b2239e0a9539abde9a523a4a41349c82200caf06
Jesse Abdul <jesse.abdul@noaa.gov>
2026-02-20 14:39:17 -1000

    template update

--- container_database_deployment_template/deployment_scripts/client_scripts/database_deployment.sh
+++ container_database_deployment_template/deployment_scripts/client_scripts/database_deployment.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$(realpath "${0}")")"
 source ./includes/include_client_resources.sh
 
 # initialize the deployment script
-initialize_deployment_script "${0}"
+initialize_deployment_script "${DEPLOYMENT_SCRIPT_LOGS}" "${0}"
 
 # prepare and execute the deployment script
 time client_deploy_database "${1:-}" "${2:-}" "${3:-}"
