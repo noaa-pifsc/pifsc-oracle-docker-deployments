c32a4b754204f1b608bc9f5fc94e974613685b70
Jesse Abdul <jesse.abdul@noaa.gov>
2026-02-24 10:21:16 -1000

    Removed configuration variables that depend on project-specific variables so they can be defined in the individual projects

--- src/config/container_config.sh
+++ src/config/container_config.sh
@@ -2,17 +2,6 @@
 
 # define a list of configuration variables that drive the behavior of the oracle container deployment scripts 
 
-##### Container Host Configuration Variables: #####
-
-	# define the container source directory that will be created on the container host by cloning the project repository
-	CONTAINER_HOST_PROJECT_PATH="/tmp/${CONTAINER_PROJECT_FOLDER}"
-
-	# define the container source directory that contains the container source files (e.g. Dockerfile, docker-compose.yml)
-	CONTAINER_HOST_SOURCE_PATH="${CONTAINER_HOST_PROJECT_PATH}/container_database_deployment"
-
-	# define the path to the folder where the host bash scripts are contained
-	CONTAINER_HOST_SCRIPTS_PATH="${CONTAINER_HOST_PROJECT_PATH}/container_database_deployment/deployment_scripts/host_scripts"
-
 ##### Container Configuration Variables: #####
 
 	# define the container's root folder where the source files are copied
