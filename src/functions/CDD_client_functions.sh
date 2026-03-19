4a15ddff442150d96cc6e712610b9667194a2369
Jesse Abdul <jesse.abdul@noaa.gov>
2026-03-18 17:35:39 -1000

    updated runtime variable output during execution

--- src/functions/CDD_client_functions.sh
+++ src/functions/CDD_client_functions.sh
@@ -67,7 +67,7 @@ function cdd_client_process_runtime_arguments ()
 	# Store the validated value back into the arguments array
 	cds_shared_set_array_val "${arg_array}" "container_script_type" "${local_script_type}"
 
-	echo "The runtime arguments are:"
+	echo "Runtime Argument Values:"
 	echo "container_env_name: $(cds_shared_get_array_val "${arg_array}" "container_env_name")"
 	echo "container_deploy_dest: $(cds_shared_get_array_val "${arg_array}" "container_deploy_dest")"
 	echo "container_script_type: ${local_script_type}"
